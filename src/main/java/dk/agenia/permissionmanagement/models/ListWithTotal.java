dd19c351a0cbac6ab323062413bdf81f0755b366
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2018-10-08 10:52:08 +0200

    made authService an interface. this is an optional interface

--- src/main/java/dk/agenia/permissionmanagement/models/ListWithTotal.java
+++ src/main/java/dk/agenia/permissionmanagement/models/ListWithTotal.java
@@ -1,11 +1,5 @@
 package dk.agenia.permissionmanagement.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
